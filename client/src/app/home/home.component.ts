b54622a49bd72cdd7e037b0741bfb786638ec1d4
Greg <gregcummines@gmail.com>
2021-04-25 16:16:14 -0500

    add websocket client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommandCenterService } from './command-center.service';
+import { WebSocketSubject } from 'rxjs/webSocket';
+
+export class Message {
+  constructor(
+      public sender: string,
+      public content: string,
+      public isBroadcast = false,
+  ) { }
+}
 
 @Component({
   selector: 'app-home',
@@ -8,9 +17,22 @@ import { CommandCenterService } from './command-center.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private commandCenterService: CommandCenterService) { }
+  constructor(private commandCenterService: CommandCenterService) { 
+    this.socket$ = new WebSocketSubject('ws://192.168.0.188:5002');
+
+    this.socket$
+        .subscribe(
+          (message) => {
+            console.log(`Message received: ${message}`);
+          },
+          (err) => console.error(err),
+          () => console.warn('Completed!')
+        );
+  }
   public temp: number;
   public error: string;
+  private socket$: WebSocketSubject<Message>;
+
   ngOnInit(): void {
     this.commandCenterService.getTemp().subscribe(
       data => {
