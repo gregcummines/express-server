9c2050b83cac806cbbf1db04d394ad23cb6aea3c
Greg <gregcummines@gmail.com>
2021-04-25 16:55:38 -0500

    send temp every 5 secs

--- server/src/websockets/websocket-server.ts
+++ server/src/websockets/websocket-server.ts
@@ -52,8 +52,10 @@ export class WebSocketServer {
         });
     
         //send immediatly a feedback to the incoming connection    
-        ws.send(this.createMessage(readAllF(3)[0].t.toString()));
-    
+        setInterval(() => {
+            ws.send(this.createMessage(readAllF(1)[0].t.toString()));
+        }, 5000);
+        
         ws.on('error', (err) => {
             console.warn(`Client disconnected - reason: ${err}`);
         })
