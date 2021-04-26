cb506c839b82eed7572d2c207d2c75338437d1ce
Greg Cummines <greg_cummines@starkey.com>
2021-04-26 07:42:45 -0500

    Remove old api

--- api-old/routes/command-center.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controller');
-
-router.get('/get-temp', (req, res, next) => {
-  res.json(controller.getTemp());
-});
-
-module.exports = router;
