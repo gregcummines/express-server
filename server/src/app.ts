46696d327e14febd659fb06497f5793662f333c2
Greg Cummines <greg_cummines@starkey.com>
2021-04-30 14:25:59 -0500

    upgrade listen so that websocket can make a connection.

--- server/src/app.ts
+++ server/src/app.ts
@@ -57,8 +57,13 @@ class App {
   }
  
   public listen() {
-    this.app.listen(this.port, () => {
+    const server = this.app.listen(this.port, () => {
       console.log(`App listening on the port ${this.port}`);
+    });
+    server.on('upgrade', (request, socket, head) => {
+      this.wss.getWss().handleUpgrade(request, socket, head, socket => {
+        this.wss.getWss().emit('connection', socket, request);
+      });
     });
   }
 }
