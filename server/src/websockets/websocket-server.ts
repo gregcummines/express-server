e414fa61886f36e0b6fb706c4e3c48eb9694e31a
Greg Cummines <greg_cummines@starkey.com>
2021-05-01 08:52:32 -0500

    fix up setinterval

--- server/src/websockets/websocket-server.ts
+++ server/src/websockets/websocket-server.ts
@@ -60,19 +60,21 @@ export class WebSocketServer {
   }
 
   startMonitoringTemperatures() {
-    let tempSensors = readAllF(1, (err: string, results: ValueWithID[]) => {
-        if (err) {
-            console.warn(err);
-        } else {
-            results.forEach( (sensor) => {
-                const storedSensor = this.tempSensors.find(element => element.id === sensor.id);
-                if (storedSensor) {
-                    storedSensor.t = sensor.t;
-                } else {
-                    this.tempSensors.push(sensor);
-                }
-            });
-        }
-    });
+    setInterval(() => {
+        const tempSensors = readAllF(1, (err: string, results: ValueWithID[]) => {
+            if (err) {
+                console.warn(err);
+            } else {
+                results.forEach( (sensor) => {
+                    const storedSensor = this.tempSensors.find(element => element.id === sensor.id);
+                    if (storedSensor) {
+                        storedSensor.t = sensor.t;
+                    } else {
+                        this.tempSensors.push(sensor);
+                    }
+                });
+            }
+        });
+    }, 1000);  
   }
 }
