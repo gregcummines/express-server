fb7bb0b4db39c2789cb85c7154a403ff88597062
Greg Cummines <greg_cummines@starkey.com>
2021-05-04 11:25:52 -0500

    added error handling in repository class

--- server/src/respository/walrus.ts
+++ server/src/respository/walrus.ts
@@ -3,41 +3,58 @@ import * as sqlite from 'sqlite3';
 
 export class WalrusDatabase {
     public getUser(username: string, password: string): User {
-        const db = new sqlite.Database("walrus.db");
-        const params = [username, password];
         let user: User = null;
-        db.get("SELECT * FROM user WHERE email = ? and password = ?", params, (err, row) => {
+        const db = new sqlite.Database("walrus.db", (err) => {
             if (err) {
-                console.error(err);
+              console.error('Could not connect to database', err);
             } else {
-                user = new User();
-                user.id = row.id;
-                user.firstName = row.first_name;
-                user.lastName = row.last_name;
-                user.email = row.email;
+              console.log('Connected to database');
             }
         });
-        return user;
-    } 
-
-    public getUsers(): User[] {
-        const db = new sqlite.Database("walrus.db");
-        let users: User[] = null;
-        db.all("SELECT * FROM user", (err, rows) => {
-            if (err) {
-                console.error(err);
-            } else {
-                rows.forEach(row => {
-                    let user = new User();
+        if (db) {
+            const params = [username, password];
+            
+            db.get("SELECT * FROM user WHERE email = ? and password = ?", params, (err, row) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    user = new User();
                     user.id = row.id;
                     user.firstName = row.first_name;
                     user.lastName = row.last_name;
                     user.email = row.email;
-                    users.push(user);
-                });
-                
+                }
+            });
+        }
+        return user;
+    } 
+
+    public getUsers(): User[] {
+        let users: User[] = null;
+        const db = new sqlite.Database("walrus.db", (err) => {
+            if (err) {
+              console.error('Could not connect to database', err);
+            } else {
+              console.log('Connected to database');
             }
         });
+        if (db) {
+            db.all("SELECT * FROM user", (err, rows) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    rows.forEach(row => {
+                        let user = new User();
+                        user.id = row.id;
+                        user.firstName = row.first_name;
+                        user.lastName = row.last_name;
+                        user.email = row.email;
+                        users.push(user);
+                    });
+                    
+                }
+            });
+        }
         return users;
     } 
 }
