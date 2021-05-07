4e3a5dead7836747fb59331e66eb9af037cd1ab9
Greg Cummines <greg_cummines@starkey.com>
2021-05-07 16:54:38 -0500

    check token for route guard

--- client/src/app/auth/auth-guard.service.ts
+++ client/src/app/auth/auth-guard.service.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const currentUser = this.authService.currentUserValue;
-        if (currentUser) {
+        if (currentUser && currentUser.token) {
             // logged in so return true
             return true;
         }
