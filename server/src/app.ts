6fb6de80a440cef508090c75db4cfd58faabab21
Greg Cummines <greg_cummines@starkey.com>
2021-05-11 13:46:37 -0500

    add semicolons at the end of statements.

--- server/src/app.ts
+++ server/src/app.ts
@@ -87,11 +87,11 @@ class App {
 function shouldCompress (req, res) {
   if (req.headers['x-no-compression']) {
     // don't compress responses with this request header
-    return false
+    return false;
   }
  
   // fallback to standard filter function
-  return compression.filter(req, res)
+  return compression.filter(req, res);
 }
 
 export default App;
